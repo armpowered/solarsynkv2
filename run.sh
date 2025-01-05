7078e43ce177b3556e02fa44ddb6a8975757a1b9
armpowered <123898199+armpowered@users.noreply.github.com>
2025-01-05 15:41:31 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -48,6 +48,7 @@ echo "Bearer Token length:" ${#ServerAPIBearerToken}
 
 #timbo
 if [ ${#ServerAPIBearerToken} -eq 4]
+then
       echo "Retry server api bearer token fetch"
       ServerAPIBearerToken=$(curl -s -k -X POST -H "Content-Type: application/json" https://api.sunsynk.net/oauth/token -d '{"areaCode": "sunsynk","client_id": "csp-web","grant_type": "password","password": "'"$sunsynk_pass"'","source": "sunsynk","username": "'"$sunsynk_user"'"}' | jq -r '.data.access_token')
 fi
