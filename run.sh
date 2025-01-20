1ec98ea6368c8ea8e399644b7290507828ccb579
armpowered <123898199+armpowered@users.noreply.github.com>
2025-01-20 15:01:46 +0000

    Update run.sh
    Exit on null token

--- run.sh
+++ run.sh
@@ -48,9 +48,12 @@ echo "Bearer Token length:" ${#ServerAPIBearerToken}
 
 #timbo
 if [ "${#ServerAPIBearerToken}" -eq "4" ]
+# i.e. null
 then
-      echo "Retry server api bearer token fetch"
-      ServerAPIBearerToken=$(curl -s -k -X POST -H "Content-Type: application/json" https://api.sunsynk.net/oauth/token -d '{"areaCode": "sunsynk","client_id": "csp-web","grant_type": "password","password": "'"$sunsynk_pass"'","source": "sunsynk","username": "'"$sunsynk_user"'"}' | jq -r '.data.access_token')
+      #echo "Retry server api bearer token fetch"
+      echo "Null token, exiting"
+      exit
+      #ServerAPIBearerToken=$(curl -s -k -X POST -H "Content-Type: application/json" https://api.sunsynk.net/oauth/token -d '{"areaCode": "sunsynk","client_id": "csp-web","grant_type": "password","password": "'"$sunsynk_pass"'","source": "sunsynk","username": "'"$sunsynk_user"'"}' | jq -r '.data.access_token')
 fi
 
 #timboend
